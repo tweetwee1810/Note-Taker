0943af92c1fabd08c88d9bcab14354f8c50514b1
Mona Lisa <tweetwee1996@gmail.com>
2023-02-02 18:05:37 -0800

    debug

--- routes/router.js
+++ routes/router.js
@@ -1,17 +1,23 @@
 const router = require('express').Router()
 const fs = require('fs');
 const path = require('path');
+
+//api endpoint for notes
 router.get('/notes', (req, res) => {
     fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
-        let notes = JSON.parse(data);
-        res.status(200).json(notes)
+        if (err) {
+            res.status(500).json({ error: 'Error reading notes file' });
+        } else {
+            let notes = JSON.parse(data);
+            res.status(200).json(notes)
+        }
     })
-})
- //retuns notes from db.json
+});
+//retuns notes from db.json
 router.post('/notes', (req, res) => {
     fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8', (err, data) => {
         if (err) {
-            res.status(500).send('Error reading notes file');
+            res.status(500).send({ error: 'Error reading notes file' });
         } else {
             let notes = JSON.parse(data);
             const newNote = req.body;
@@ -19,9 +25,9 @@ router.post('/notes', (req, res) => {
             notes.push(newNote);
             fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
                 if (err) {
-                    res.status(500).send('Error writing to notes file');
+                    res.status(500).send({ error: 'Error writing to notes file' });
                 } else {
-                    res.status(200).send(newNote);
+                    res.status(200).json(newNote);
                 }
             });
         }
@@ -31,21 +37,25 @@ router.post('/notes', (req, res) => {
 
 //delete 
 router.delete('/notes/:id', (req, res) => {
-    const noteId = req.params.id;
+    const noteId = parseInt(req.params.id);
     console.info(`${req.method} deleted request was called`);
     fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
-      let notes = JSON.parse(data);
-      const remainingNotes = notes.filter(note => note.id !== noteId);
-      fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(remainingNotes), (err) => {
         if (err) {
-          res.status(500).json({ error: 'Failed to delete note' });
-          return;
+            res.status(500).json({ error: 'Error reading notes file' });
+        } else {
+            let notes = JSON.parse(data);
+            const remainingNotes = notes.filter(note => note.id !== noteId);
+            fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(remainingNotes), (err) => {
+                if (err) {
+                    res.status(500).json({ error: 'Failed to delete note' });
+                    return;
+                }
+                res.status(200).json({ message: 'Note deleted successfully' });
+            });
         }
-        res.status(200).json({ message: 'Note deleted successfully' });
-      });
     });
-  });
-  
+});
+
 
 // let deleteData = fs.readFileSync('./db/db.json');
 module.exports = router;
